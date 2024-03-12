9af907358de37a026c8e40d588760866783d3af1
Stephanie Lu <lu.stephanie.lu@gmail.com>
2024-03-11 20:28:58 -0400

    Add equally weighted portfolio

--- Project 1 MATLAB/MIE377 - Project 1 Files/services/project_function.py
+++ Project 1 MATLAB/MIE377 - Project 1 Files/services/project_function.py
@@ -1,6 +1,17 @@
 from services.strategies import *
 
 
+# def project_function(periodReturns, periodFactRet):
+#     """
+#     Please feel free to modify this function as desired
+#     :param periodReturns:
+#     :param periodFactRet:
+#     :return: the allocation as a vector
+#     """
+#     Strategy = OLS_MVO()
+#     x = Strategy.execute_strategy(periodReturns, periodFactRet)
+#     return x
+
 def project_function(periodReturns, periodFactRet):
     """
     Please feel free to modify this function as desired
@@ -8,6 +19,9 @@ def project_function(periodReturns, periodFactRet):
     :param periodFactRet:
     :return: the allocation as a vector
     """
-    Strategy = OLS_MVO()
-    x = Strategy.execute_strategy(periodReturns, periodFactRet)
+
+    # Equal weight portfolio 
+    x = equal_weight(periodReturns)
+    
     return x
+
