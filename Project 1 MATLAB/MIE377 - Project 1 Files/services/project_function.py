ac7c0d4ef673c1b800d1f9660f4949eac365199b
esther9089 <123838902+esther9089@users.noreply.github.com>
2024-03-14 13:59:29 -0400

    update template

--- Project 1 MATLAB/MIE377 - Project 1 Files/services/project_function.py
+++ Project 1 MATLAB/MIE377 - Project 1 Files/services/project_function.py
@@ -1,7 +1,7 @@
 from services.strategies import *
 
 
-def project_function(periodReturns, periodFactRet):
+def project_function(periodReturns, periodFactRet, x0):
     """
     Please feel free to modify this function as desired
     :param periodReturns:
