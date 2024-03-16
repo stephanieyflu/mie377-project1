b287b839c4b0b79da2afe07bcd85741f0dbb9534
Aarya <143751805+23003aary@users.noreply.github.com>
2024-03-16 11:52:30 -0400

    Added Max_Sharpe_Ratio related functions (Roboust and Minimizing turnover) documented results as well

--- Project 1 MATLAB/MIE377 - Project 1 Files/services/optimization.py
+++ Project 1 MATLAB/MIE377 - Project 1 Files/services/optimization.py
@@ -47,3 +47,109 @@ def MVO(mu, Q):
 
     prob.solve(verbose=False, solver=cp.ECOS)
     return x.value
+
+
+#edit so you can construct MVO 
+def Sharpe_MVO(mu, Q):
+        # Find the total number of assets
+    n = len(mu)
+
+    # Define and solve using CVXPY
+    y = cp.Variable(n)
+    k = cp.Variable()
+    
+
+    prob = cp.Problem(cp.Minimize(cp.quad_form(y, Q)), 
+                      [np.transpose(mu)@y == 1,
+                       np.transpose(np.ones(n))@y == k,
+                       k >= 0,
+                       y >= 0])
+
+    
+    # change verbose to True to output optimization
+    # info to console
+
+    prob.solve(verbose=False, solver=cp.ECOS)
+    x = y.value/k.value
+
+    return x
+
+def Robust_Sharpe_Eps_MVO(mu, Q, T): #NOT USING
+           # Find the total number of assets
+    n = len(mu)
+
+    # Set the target as the average expected return of all assets
+    targetRet = np.mean(mu)
+
+    # Define and solve using CVXPY
+    y = cp.Variable(n)
+    eps = 1.96
+    theta = np.sqrt(np.diag(Q)/T)
+    k = np.transpose(np.ones(n))@y 
+
+    prob = cp.Problem(cp.Minimize(cp.quad_form(y, Q)), 
+                      [(mu.T)@y - eps*cp.norm((theta)@y) >= 1, #form of problem in objective
+                       cp.sum(y) >= 0,
+                       y >= 0])
+    # change verbose to True to output optimization
+    # info to console
+
+    prob.solve(verbose=True, solver=cp.ECOS)
+    x = y.value/k.value
+
+    return x
+
+def Max_Sharpe_Min_Turn_1(mu, Q, x0, llambda=2):
+    #towards minimizing turnover
+        # Find the total number of assets
+    n = len(mu)
+
+    # Define and solve using CVXPY
+    y = cp.Variable(n)
+    k = cp.Variable()
+    z = cp.Variable(n)
+     #scalar big just turn over, and small is sharpe 
+    
+    print(x0.shape)
+    print(k)
+    prob = cp.Problem(cp.Minimize(cp.quad_form(y, Q)+(llambda*cp.sum(z))), 
+                      [np.transpose(mu)@y == 1,
+                       np.transpose(np.ones(n))@y == k,
+                       z >= y - (k*x0),
+                       z >= (k*x0) -y,
+                       k >= 0,
+                       y >= 0])
+    
+    prob.solve(verbose=True, solver=cp.ECOS)
+    x = y.value/k.value
+
+    return x
+
+def Max_Sharpe_Min_Turn_2(mu, Q, x0, llambda = 0.5):
+    #towards maximizing sharpe
+        # Find the total number of assets
+    n = len(mu)
+
+    # Define and solve using CVXPY
+    y = cp.Variable(n)
+    k = cp.Variable()
+    z = cp.Variable(n)
+     #scalar big just turn over, and small is sharpe 
+    
+    print(x0.shape)
+    print(k)
+    prob = cp.Problem(cp.Minimize(cp.quad_form(y, Q)+(llambda*cp.sum(z))), 
+                      [np.transpose(mu)@y == 1,
+                       np.transpose(np.ones(n))@y == k,
+                       z >= y - (k*x0),
+                       z >= (k*x0) -y,
+                       k >= 0,
+                       y >= 0])
+    
+    prob.solve(verbose=True, solver=cp.ECOS)
+    x = y.value/k.value
+
+    return x
+    
+
+
