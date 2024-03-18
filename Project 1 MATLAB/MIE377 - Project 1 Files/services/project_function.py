4b06c85f2d2a90e186a1cfc2ca2d8ebda2f9dfa2
Stephanie Lu <lu.stephanie.lu@gmail.com>
2024-03-18 18:37:34 -0400

    Delete files

--- Project 1 MATLAB/MIE377 - Project 1 Files/services/project_function.py
+++ Project 1 MATLAB/MIE377 - Project 1 Files/services/project_function.py
@@ -12,7 +12,7 @@ def project_function(periodReturns, periodFactRet, x0):
     :param periodFactRet:
     :return: the allocation as a vector
     """
-    Strategy = PCA_MVO() # Vary parameters
+    Strategy = PCA_MVO()
 
     # Check if we are in the calibration period
     T, n = periodReturns.shape
